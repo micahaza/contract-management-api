aec3b866812c569af9c1894054f19a451f917383
Pető Zoltán <bajgli@gmail.com>
2019-08-27 14:18:29 +0200

    jsonized responses

--- jcapi/blueprints/registration.py
+++ jcapi/blueprints/registration.py
@@ -1,22 +1,19 @@
-from flask import Blueprint, request, render_template, jsonify
+from flask import Blueprint, request, jsonify
 from jcapi.models import User
 from jcapi import db
 
 registration = Blueprint('registration', __name__)
 
 
-@registration.route('/', methods=['GET', 'POST'])
+@registration.route('/', methods=['POST'])
 def register():
-    if request.method == 'GET':
-        return render_template('registration.html')
-    elif request.method == 'POST':
-        req_data = request.get_json()
-        username = req_data['username']
-        email = req_data['email']
-        password = req_data['password']
-        u_exists = User.query.filter_by(username=username).first()
-        if u_exists is None:
-            u = User(username, email, password)
-            db.session.add(u)
-            db.session.commit()
-        return jsonify({}), 204
+    req_data = request.get_json()
+    username = req_data['username']
+    email = req_data['email']
+    password = req_data['password']
+    u_exists = User.query.filter_by(username=username).first()
+    if u_exists is None:
+        u = User(username, email, password)
+        db.session.add(u)
+        db.session.commit()
+    return jsonify({}), 204
--- tests/functional/registration_test.py
+++ tests/functional/registration_test.py
@@ -1,10 +1,10 @@
-def test_page_loads(test_client):
-    response = test_client.get('/registration/', follow_redirects=True)
-    assert response.status_code == 200
-    assert b"Registration" in response.data
+from jcapi.models import User
 
 
 def test_user_can_register(test_client):
-    reg_data = dict(username='asdf', email='adsf@asdf.hu', password='asdf')
+    reg_data = dict(username='asdf', email='asdf@asdf.hu', password='asdf')
     response = test_client.post('/registration/', json=reg_data, follow_redirects=True)
     assert response.status_code == 204
+    new_user = User.query.filter_by(username='asdf').first()
+    assert new_user.email == 'asdf@asdf.hu'
+    assert new_user.password_hash is not None
